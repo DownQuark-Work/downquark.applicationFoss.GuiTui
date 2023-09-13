dcd5ad631372f0095d7b9cb26943ca54d873c083
mlnck <jack@melnick.>
2023-09-13 06:18:14 -0400

    async foreach working

--- shell/_utils/fs.ts
+++ shell/_utils/fs.ts
@@ -10,19 +10,18 @@ const validateVersion = (persistedVersion:string) => {
 
 export const loadSrcFile = (path:string) => Deno.readTextFileSync(path)
 
-const loadScriptByType = (fileName:string) => {
-  const isDynamicImport = fileName.split('.').pop() === 'ts'
-  console.log('fileName: ', fileName,loadSrcFile(fileName))
-  
-  // return Promise.resolve({a:'b'})
-  return isDynamicImport
-    ? import('../'+fileName)
-    : loadSrcFile(fileName)
-}
-const asyncForEach = async(array, callback) => {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index]);
+const asyncForEach = async (arr, cb) => {
+  for (let i = 0; i < arr.length; i++) {
+    await cb(arr[i]);
   }
+}
+
+const loadScriptByType = async (fileName:string) => {
+  const isDynamicImport = fileName.split('.').pop() === 'ts'
+  
+  return isDynamicImport
+    ? await import('../'+fileName)
+    : loadSrcFile(fileName)
 }
 export const LoadDynamicScripts = async () => {
   if(Object.keys(SECTION_SCRIPTS).length) return // failsafe: scripts have already been loaded
@@ -35,18 +34,13 @@ export const LoadDynamicScripts = async () => {
     SECTION_SCRIPTS._global = await loadScriptByType(srcDir+_BLUEPRINT.TUI.CONFIG?.SCRIPT_SOURCE)
     
   
-  const sectionScripts = []
-  _BLUEPRINT.TUI.sections.forEach((section) => {
-    // if(section.scriptSrc) // load scripts for all applicable sections
-    section.scriptSrc && sectionScripts.push([section.id,section.scriptSrc])
-      // SECTION_SCRIPTS[section.id] = await loadScriptByType(srcDir+section.scriptSrc)
-  })
+  const sectionScripts:Array<[string,string]> = []
+  _BLUEPRINT.TUI.sections.forEach((section) => 
+    section.scriptSrc && sectionScripts.push([section.id,section.scriptSrc]))
 
   const asyncForEachWrapper = async () => {
-    await asyncForEach(sectionScripts,async (curScript) => {
-      SECTION_SCRIPTS[curScript[0]] = await loadScriptByType(srcDir+curScript[1])
-      console.log('INSIDE SECTION_SCRIPTS: ', SECTION_SCRIPTS)
-    })
+    await asyncForEach(sectionScripts,async (curScript:[string,string]) => 
+      SECTION_SCRIPTS[curScript[0]] = await loadScriptByType(srcDir+curScript[1]))
     parseDynamicScripts()
   }
   asyncForEachWrapper()
