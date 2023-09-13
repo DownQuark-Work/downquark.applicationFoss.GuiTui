9f2299360973fe4b63e829bc5b935c22ef46b775
mlnck <jack@melnick.>
2023-09-13 02:32:30 -0400

    tab working to switch sections

--- shell/tui/blueprint.actions.ts
+++ shell/tui/blueprint.actions.ts
@@ -4,17 +4,8 @@ import { RESERVED_KEYPRESS, processKeyPress } from "../interactions/keyboard.ts"
 import { keyPressReturnType } from "../interactions/keyboard.ts";
 import { Content } from "./blueprint.content.ts";
 
-const cycleActiveSection = () => {
-  Content.Set.idActive()
-}
-
 const handleParsedKeyboardEvent = (key:keyPressReturnType) => {
-  // console.clear()
-  // Grid.Render()
-  // console.log('ApplyActions _BLUEPRINT: ', _BLUEPRINT)
-  console.log('handleParsedKeyboardEvent: ', key)
-  console.log('key.isKey(): ', key.isKey(RESERVED_KEYPRESS.TAB))
-  key.isKey(RESERVED_KEYPRESS.TAB) && cycleActiveSection()
+  key.isKey(RESERVED_KEYPRESS.TAB) && Content.Set.idActive()
 }
 
 export const ApplyActions = () => {
