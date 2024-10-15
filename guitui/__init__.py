bbf56330b77aa6c847c850893a5e84209c9ceaf4
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-10-15 01:19:26 -0400

    more

--- guitui/__init__.py
+++ guitui/__init__.py
@@ -1,4 +1,5 @@
 """GUITUI base initialization"""
 
-from guitui.__main__ import set_terminal_size
+from guitui.__main__ import set_terminal_grid
 
+full_terminal_grid_data = set_terminal_grid()
--- guitui/__main__.py
+++ guitui/__main__.py
@@ -1,11 +1,15 @@
-# for architecture/scaffolding
-## https://github.com/scrapy/scrapy/blob/master/scrapy
-
 from utils import get_terminal_size
+
+def set_terminal_grid():
+     (columns, lines) = get_terminal_size()
+     ret_obj = {
+         'cols':columns,
+         'rows':lines,
+         'tot':columns*lines
+     }
+     return ret_obj
 
 if __name__ == '__main__':
     print('proof of concept::running')
     print('get_terminal_size', get_terminal_size())
-
-def set_terminal_size():
-    return get_terminal_size()
+    print('set_terminal_grid',set_terminal_grid())
