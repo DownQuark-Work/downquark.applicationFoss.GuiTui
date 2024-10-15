28edb88f72c7cf61a4e39edd838085606ef170cf
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-10-15 02:26:04 -0400

    indexing

--- guitui/__main__.py
+++ guitui/__main__.py
@@ -1,4 +1,4 @@
-from guitui.utils import get_terminal_size
+from utils import get_terminal_size, determine_tui_section_indexes
 
 def set_terminal_grid():
      (columns, lines) = get_terminal_size()
@@ -13,3 +13,4 @@ if __name__ == '__main__':
     print('proof of concept::running')
     print('get_terminal_size', get_terminal_size())
     print('set_terminal_grid',set_terminal_grid())
+    print('determine_tui_section_indexes', determine_tui_section_indexes(set_terminal_grid()))
--- guitui/utils/__init__.py
+++ guitui/utils/__init__.py
@@ -6,6 +6,10 @@ def get_terminal_size():
     print('w/hh',size)
     return size
 
+def determine_tui_section_indexes(full_grid_dimensions):
+    print('full_terminal_grid_data',full_grid_dimensions)
+    return 'determine_tui_section_indexes'
+
 def parse_toml_file(path:str):
     with open(path, "rb") as f:
         data = tomllib.load(f)
