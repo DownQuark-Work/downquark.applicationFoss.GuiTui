32c951db3be70bde5e0768259e40c44837582548
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-10-15 00:57:30 -0400

    inits

--- guitui/__init__.py
+++ guitui/__init__.py
@@ -1,1 +1,4 @@
 """GUITUI base initialization"""
+
+from guitui.__main__ import set_terminal_size
+
--- guitui/utils/__init__.py
+++ guitui/utils/__init__.py
@@ -1,5 +1,16 @@
 import os
+import tomllib
 
 def get_terminal_size():
     size = os.get_terminal_size()
     print('w/hh',size)
+    return size
+
+def parse_toml_file(path:str):
+    with open(path, "rb") as f:
+        data = tomllib.load(f)
+    return data
+
+def parse_toml_str(toml_str:str):
+    data = tomllib.loads(toml_str)
+    return data
