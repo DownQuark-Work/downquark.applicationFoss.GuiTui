a2982a2b4162efeb3c6500444dc607525b49bbcd
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-10-25 22:50:27 -0400

    misc

--- guitui/__main__.py
+++ guitui/__main__.py
@@ -9,8 +9,19 @@ def set_terminal_grid():
      }
      return ret_obj
 
+def good_fibonacci(n):
+    """Return pair of Fibonacci numbers, F(n) and F(n-1)."""
+    if n <= 1:
+        return n,0
+    else:
+        x = n - 1
+        (a, b) = good_fibonacci(x)
+    return a+b, a
+
 if __name__ == '__main__':
     print('proof of concept::running')
     print('get_terminal_size', get_terminal_size())
     print('set_terminal_grid',set_terminal_grid())
     print('determine_tui_section_indexes', determine_tui_section_indexes(set_terminal_grid()))
+    print('good_fibonacci', good_fibonacci(10))
+
