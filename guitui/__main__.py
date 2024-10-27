f07b7b6e1542250b457edbfa3e90c17a1ac5f0d4
dq-mlnck <40064794+dq-mlnck@users.noreply.github.com>
2024-10-27 01:38:37 -0400

    getting ready for teardown

--- guitui/__main__.py
+++ guitui/__main__.py
@@ -19,8 +19,21 @@ def good_fibonacci(n):
     return a+b, a
 
 if __name__ == '__main__':
+    print('before the patterns:: https://fpgmaas.github.io/cookiecutter-poetry/')
+    print('-=-=')
     print('HERE WE GO: starting point for scaffold: https://github.com/MartinHeinz/python-project-blueprint/tree/master/blueprint')
     print('and perhaps this as the followup: https://github.com/faif/python-patterns/blob/master/patterns/structural/3-tier.py')
+    # more extendables:
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chaining_method.py <- method1(0).do_method_two(2).and_another_method().do_method_two(22)
+    #  https://github.com/faif/python-patterns/blob/master/patterns/other/blackboard.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/other/hsm/hsm.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/specification.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/observer.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/publish_subscribe.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/registry.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/specification.py
+    # https://github.com/faif/python-patterns/blob/master/patterns/structural/composite.py
+
     print('proof of concept::running')
     print('get_terminal_size', get_terminal_size())
     print('set_terminal_grid',set_terminal_grid())
